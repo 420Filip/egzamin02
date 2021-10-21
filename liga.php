1629326179403edd84898b42f1eb1b19169da5b3
420Filip <f1l1pezeg@yahoo.com>
2021-10-21 12:46:14 +0200

    a

--- liga.php
+++ liga.php
@@ -8,11 +8,7 @@
     <title>piłka nożna</title>
     <link rel="stylesheet" href="styl.css">
 </head>
-
 <body>
-    <?php
-    $db = new mysqli('localhost', 'root', '', 'egzamin2');
-    ?>
     <header>
         <h3>Reprezentacja Polski w Piłce Nożnej</h3>
         <img src="obraz1.jpg" alt="reprezentacja">
@@ -20,56 +16,20 @@
     <div id="podzial">
         <div id="lewy">
             <form action="liga.php" method="post">
-                <select name="pozycja">
-                    <option value="1">Bramkarze</option>
-                    <option value="2">Obrońcy</option>
-                    <option value="3">Pomocnicy</option>
-                    <option value="4">Napastnicy</option>
-                </select>
-                <button type="submit">Zobacz</button>
+            
             </form>
             <img src="zad2.png" alt="piłka">
-            <p>Autor: Filip Chełchowski</p>
+            <p>Autor:Filip Chełchowski</p>
         </div>
         <div id="prawy">
-            <ol>
-                <?php
-                $query = $db->prepare("SELECT imie,nazwisko FROM zawodnik WHERE pozycja_id = ?");
-                $query->bind_param('i', $_POST['pozycja']);
-                $query->execute();
-                $result = $query->get_result();
-                while ($row = $result->fetch_assoc()) {
-                    echo '<li>';
-                    echo $row['imie'];
-                    echo ' ';
-                    echo $row['nazwisko'];
-                    echo '</li>';
-                }
-                ?>
-            </ol>
+        
         </div>
     </div>
     <main>
         <h3>Liga mistrzów</h3>
     </main>
     <div id="liga">
-        <?php
-            $query = $db->prepare("SELECT zespol, punkty, grupa FROM liga ORDER BY punkty DESC");
-            $query->execute();
-            $result = $query->get_result();
-            while($row = $result->fetch_assoc()) {
-                echo '<div id="druzyna">';
-                echo '<h2>'.$row['zespol'].'</h2>';
-                echo '<h1>'.$row['punkty'].'</h1>';
-                echo '<p>grupa:'.$row['grupa'].'</p>';
-                echo '</div>';
-            }
-        ?>
         
     </div>
-    <?php
-    $db->close();
-    ?>
 </body>
-
 </html>
